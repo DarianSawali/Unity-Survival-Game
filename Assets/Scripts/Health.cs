39d6e57afa1859ba7868c88bfba3ddf12b6b7a42
Ry-an7 <117701680+Ry-an7@users.noreply.github.com>
2023-10-07 21:56:48 -0700

    Added new health bar mechanic

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public GameObject Heart1;
+    public GameObject Heart2;
+    public GameObject Heart3;
+    public GameObject Heart4;
+    public GameObject Heart5;
+
+    int health = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            health--;
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            health++;
+        }
+        switch(health) {
+            case 0: {
+                Heart1.gameObject.SetActive(false);
+                Heart2.gameObject.SetActive(false);
+                Heart3.gameObject.SetActive(false);
+                Heart4.gameObject.SetActive(false);
+                Heart5.gameObject.SetActive(false);
+                break;
+            }
+            case 1: {
+                Heart1.gameObject.SetActive(true);
+                Heart2.gameObject.SetActive(false);
+                Heart3.gameObject.SetActive(false);
+                Heart4.gameObject.SetActive(false);
+                Heart5.gameObject.SetActive(false);
+                break;
+            }
+            case 2: {
+                Heart1.gameObject.SetActive(true);
+                Heart2.gameObject.SetActive(true);
+                Heart3.gameObject.SetActive(false);
+                Heart4.gameObject.SetActive(false);
+                Heart5.gameObject.SetActive(false);
+                break;
+            }
+            case 3: {
+                Heart1.gameObject.SetActive(true);
+                Heart2.gameObject.SetActive(true);
+                Heart3.gameObject.SetActive(true);
+                Heart4.gameObject.SetActive(false);
+                Heart5.gameObject.SetActive(false);
+                break;
+            }
+            case 4: {
+                Heart1.gameObject.SetActive(true);
+                Heart2.gameObject.SetActive(true);
+                Heart3.gameObject.SetActive(true);
+                Heart4.gameObject.SetActive(true);
+                Heart5.gameObject.SetActive(false);
+                break;
+            }
+            case 5: {
+                Heart1.gameObject.SetActive(true);
+                Heart2.gameObject.SetActive(true);
+                Heart3.gameObject.SetActive(true);
+                Heart4.gameObject.SetActive(true);
+                Heart5.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+}
